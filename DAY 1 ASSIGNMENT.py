18ce9bc5aafc7217065f97f8a252af413ec228f9
abcd1509 <66481672+abcd1509@users.noreply.github.com>
2021-02-23 20:08:28 +0530

    Assignment 1 submitted

---
+++ DAY 1 ASSIGNMENT.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[3]:
+
+
+#Question 1
+#Write a python program that takes two numbers as the input and print the result of Addition,Subtraction, Multiplication, Division of the two numbers
+x=int(input('enter first number:'))
+y=int(input('enter second number:'))
+print('Sum:',x+y)
+print('Difference:',x-y)
+print('Product:',x*y)
+print("Division:",x/y)    
+
+
+# In[6]:
+
+
+#Question 2
+#Write a python program that takes two numbers as the input such as X and Y and print the result of X^Y(X to the power of Y).
+x=int(input('enter first number:'))
+y=int(input('enter second number:'))
+print('x^y:',x**y)
+
+
+# In[ ]:
+
+
+
+
